1dcc14585ddca4639bff8bc404946ac8c0939275
lxis <lxis@163.com>
2014-07-30 10:16:19 +0800

    + finish implementation

--- SageNavigation/src/com/sage/navigation/BaseActivity.java
+++ SageNavigation/src/com/sage/navigation/BaseActivity.java
@@ -1,13 +1,19 @@
 package com.sage.navigation;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.Random;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+
+
+
 
 
 import com.google.gson.Gson;
@@ -23,8 +29,15 @@ public class BaseActivity extends Activity
 	protected void onActivityResult(int requestCode, int resultCode, Intent data){	
 		super.onActivityResult(requestCode, resultCode, data);		
 		Bundle bundle = data.getExtras();
-		String resultType = bundle.getString("ResultType");
-		String result = bundle.getString("Result");
+		String fromType = bundle.getString("FromType");
+		String resultTypeText = bundle.getString("ResultType");
+		String resultText = bundle.getString("Result");
+		
+		Type resultType = new Gson().fromJson(resultTypeText,Type.class);
+		Object result = new Gson().fromJson(resultText, resultType);
+		
+		CommonSimpleHandlerGenic<Object> handler = handlers.get(fromType);
+		handler.Run(result);
 //		new Gson().fromJson(json, typeOfT) result
 //		CommonSimpleHandlerGenic<Object> handler = Result.get(resultType);
 //		handler.Run(item);
@@ -36,23 +49,29 @@ public class BaseActivity extends Activity
 		return new NavigationInterface(this,classType);
 	}
 	
-	public void go(Class NavigateClass,Object Param,HashMap<Class, CommonSimpleHandlerGenic<Object>> Handlers)
+	HashMap<String, CommonSimpleHandlerGenic<Object>> handlers = new HashMap<String, CommonSimpleHandlerGenic<Object>>();
+	
+	public void go(Class NavigateClass,Object Param,HashMap<Class, CommonSimpleHandlerGenic<Object>> handlers)
 	{		
 		Intent intent = new Intent(this,NavigateClass);
 		Bundle b = new Bundle(); 		
 		b.putString("NavigateParam", new Gson().toJson(Param));
-		int key = new Random().nextInt();
-		b.putInt("Result", key);//避免重复
 		intent.putExtras(b);
-//		Result.put(key, handler);
-		this.startActivityForResult(intent,key);
+		
+		Iterator<Entry<Class, CommonSimpleHandlerGenic<Object>>> entryKeyIterator = handlers.entrySet().iterator();
+		       while (entryKeyIterator.hasNext()) {
+		            Entry<Class, CommonSimpleHandlerGenic<Object>> e = entryKeyIterator.next();
+		            this.handlers.put(e.getKey().getName(), e.getValue());		            
+		 }
+		this.startActivityForResult(intent,1);
 	}	
 	
 	public void setResult(Object object)
 	{
 		Intent intent = new Intent();
-		Bundle bundle = new Bundle();
-		bundle.putString("ResultType",  object.getClass().getName() );
+		Bundle bundle = new Bundle();				
+		bundle.putString("FromType", this.getClass().getName());
+		bundle.putString("ResultType", new Gson().toJson(object.getClass()));
 		bundle.putString("Result",new Gson().toJson(object));
 		intent.putExtras(bundle);
 		setResult(1, intent);
